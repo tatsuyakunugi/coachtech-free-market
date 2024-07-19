440e3971cb39b1eda28755c3cbdf3f8858eabb25
tatsuya <quercus.acutissima.tree@gmail.com>
2024-07-19 14:10:53 +0900

    プロフィール編集ページ作成

--- src/resources/views/profile.blade.php
+++ src/resources/views/profile.blade.php
@@ -5,5 +5,80 @@
 @endsection
 
 @section('content')
-<p>プロフィール編集ページ</p>
+<div class="profile__content">
+    <div class="profile__heading">
+        <p>プロフィール設定</p>
+    </div>
+    <form class="profile-form" action="" method="post">
+        @csrf
+        <div class="form__group">
+            <div class="form__group-title">
+                <span class="form__label"></span>
+            </div>
+            <div class="form__group-content">
+                <div class="form__input--file">
+                    <input type="file" name="">
+                </div>
+            </div>
+            <div class="form__error">
+                <!--ここにバリデーション-->
+            </div>
+        </div>
+        <div class="form__group">
+            <div class="form__group-title">
+                <span class="form__label">ユーザー名</span>
+            </div>
+            <div class="form__group-content">
+                <div class="form__input--name">
+                    <input type="" name="">
+                </div>
+            </div>
+            <div class="form__error">
+                <!--ここにバリデーション-->
+            </div>
+        </div>
+        <div class="form__group">
+            <div class="form__group-title">
+                <span class="form__label">郵便番号</span>
+            </div>
+            <div class="form__group-content">
+                <div class="form__input--postcode">
+                    <input type="" name="">
+                </div>
+            </div>
+            <div class="form__error">
+                <!--ここにバリデーション-->
+            </div>
+        </div>
+        <div class="form__group">
+            <div class="form__group-title">
+                <span class="form__label">住所</span>
+            </div>
+            <div class="form__group-content">
+                <div class="form__input--address">
+                    <input type="" name="">
+                </div>
+            </div>
+            <div class="form__error">
+                <!--ここにバリデーション-->
+            </div>
+        </div>
+        <div class="form__group">
+            <div class="form__group-title">
+                <span class="form__label">建物名</span>
+            </div>
+            <div class="form__group-content">
+                <div class="form__input--building">
+                    <input type="" name="">
+                </div>
+            </div>
+            <div class="form__error">
+                <!--ここにバリデーション-->
+            </div>
+        </div>
+        <div class="form__button">
+            <button class="form__button-submit" type="submit">更新する</button>
+        </div>
+    </form>
+</div>
 @endsection
