10bf009c2c2ba74a0fa2859f6fc87e02d89e267c
tatsuya <quercus.acutissima.tree@gmail.com>
2024-07-19 14:05:36 +0900

    住所変更用ページ作成

--- src/resources/views/address.blade.php
+++ src/resources/views/address.blade.php
@@ -13,7 +13,56 @@
         <div class="header__inner"></div>
     </header>
     <main>
-        
+        <div class="address__content">
+            <div class="address__heading">
+                <p>住所の変更</p>
+            </div>
+            <form class="adderss-form" action="" method="post">
+                @csrf
+                <div class="form__group">
+                    <div class="form__group-title">
+                        <span class="form__label">郵便番号</span>
+                    </div>
+                    <div class="form__group-content">
+                        <div class="form__input--postcode">
+                            <input type="" name="">
+                        </div>
+                    </div>
+                    <div class="form__error">
+                        <!--ここにバリデーション-->
+                    </div>
+                </div>
+                <div class="form__group">
+                    <div class="form__group-title">
+                        <span class="form__label">住所</span>
+                    </div>
+                    <div class="form__group-content">
+                        <div class="form__input--address">
+                            <input type="" name="">
+                        </div>
+                    </div>
+                    <div class="form__error">
+                        <!--ここにバリデーション-->
+                    </div>
+                </div>
+                <div class="form__group">
+                    <div class="form__group-title">
+                        <span class="form__label">建物名</span>
+                    </div>
+                    <div class="form__group-content">
+                        <div class="form__input--building">
+                            <input type="" name="">
+                        </div>
+                    </div>
+                    <div class="form__error">
+                        <!--ここにバリデーション-->
+                    </div>
+                </div>
+                <div class="form__button">
+                    <button class="form__button-submit" type="submit">変更する</button>
+                </div>
+            </form>
+        </div>
     </main>
 </body>
 </html>
