90e31a3168c55f7a7d0b75804293152380078fad
tatsuya <quercus.acutissima.tree@gmail.com>
2024-07-19 14:08:21 +0900

    アイテム詳細ページ作成

--- src/resources/views/item.blade.php
+++ src/resources/views/item.blade.php
@@ -5,5 +5,36 @@
 @endsection
 
 @section('content')
-<p>アイテム詳細ページ</p>
+<div class="item__content">
+    <div class="item__content--inner">
+        <div class="item__image">
+            <div class="item__image-card"></div>
+        </div>
+        <div class="item__body">
+            <div class="item__body--inner">
+                <div class="item__title">
+                    <h2>商品名</h2>
+                    <p>ブランド名</p>
+                    <p>￥</p>
+                </div>
+                <div class="purchase__link-form">
+                    <a class="purchase__link" href="/purchase">購入する</a>
+                </div>
+                <div class="item__explanation">
+                    <h3>商品説明</h3>
+                    <p>カラー</p>
+                </div>
+                <div class="item__info">
+                    <h3>商品の情報</h3>
+                    <div class="category">
+                        <p class="category-tag">カテゴリー</p>
+                    </div>
+                    <div class="condition">
+                        <p class="condition-tag">商品の状態</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
