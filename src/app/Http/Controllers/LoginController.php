ea2f3f0e5d59a087fdd871e0393c1915bcec0ee8
tatsuya <quercus.acutissima.tree@gmail.com>
2024-07-20 17:51:05 +0900

    ログイン用コントローラー作成

---
+++ src/app/Http/Controllers/LoginController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    public function getLogin()
+    {
+        return view('auth.login');
+    }
+
+    public function postLogin(Request $request)
+    {
+        $user_info = $request->validate([
+            'email' => 'email|required',
+            'password' => 'required|min:8',
+        ]);
+
+        if(Auth::attempt($user_info)) {
+            $request->session()->regenerate();
+            return redirect('/');
+        }
+        return redirect()->back();
+    }
+
+    public function postLogout(Request $request)
+    {
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        return redirect('/');
+    }
+}
