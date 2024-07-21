9be7f30c766b34f9e34602a67d59c487d13cd941
tatsuya <quercus.acutissima.tree@gmail.com>
2024-07-21 18:26:27 +0900

    アクション追加

--- src/app/Http/Controllers/UserController.php
+++ src/app/Http/Controllers/UserController.php
@@ -4,8 +4,26 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use App\Models\Profile;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
-    //
+    public function mypage()
+    {
+        return view('mypage');
+    }
+
+    public function profile()
+    {
+        $user = Auth::user();
+        if(Profile::where('user_id', $user->id)->exists())
+        {
+            $profile = Profile::where('user_id', $user->id)->first();
+        }else{
+            $profile = null;
+        }
+
+        return view('profile', compact('user', 'profile'));
+    }
 }
