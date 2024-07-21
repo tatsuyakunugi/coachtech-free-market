5b3b27c6e7eeb3c5ebf20fb6f5831a378ef02a54
tatsuya <quercus.acutissima.tree@gmail.com>
2024-07-21 18:32:51 +0900

    フォームリクエスト作成

---
+++ src/app/Http/Requests/ProfileRequest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ProfileRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required',
+            'post_code' => 'required',
+            'address' => 'required',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'name.required' => 'ユーザー名を入力してください',
+            'post_code.required' => '郵便番号を入力してください',
+            'address.required' => '住所をを入力してください',
+        ];
+    }
+}
