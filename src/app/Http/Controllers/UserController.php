9f5d75b1923bef9260fc3cce0389e3db2ae8b528
tatsuya <quercus.acutissima.tree@gmail.com>
2024-07-22 17:38:35 +0900

    リンクタグ修正

--- src/app/Http/Controllers/UserController.php
+++ src/app/Http/Controllers/UserController.php
@@ -11,7 +11,14 @@ class UserController extends Controller
 {
     public function mypage()
     {
-        return view('mypage');
+        $user = Auth::user();
+        if(Profile::where('user_id', $user->id)->exists())
+        {
+            $profile = Profile::where('user_id', $user->id)->first();
+        }else{
+            $profile = null;
+        }
+        return view('mypage', compact('user', 'profile'));
     }
 
     public function profile()
