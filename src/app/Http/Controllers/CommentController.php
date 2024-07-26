43c674cc4925a5acd79c8cc4878725b554fe8870
tatsuya <quercus.acutissima.tree@gmail.com>
2024-07-26 15:42:24 +0900

    CommentController作成

---
+++ src/app/Http/Controllers/CommentController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Comment;
+
+class CommentController extends Controller
+{
+    //
+}
