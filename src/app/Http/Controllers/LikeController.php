e95fe885f68f3034d408fa2c653306359249ad51
tatsuya <quercus.acutissima.tree@gmail.com>
2024-07-26 15:43:03 +0900

    LikeController作成

---
+++ src/app/Http/Controllers/LikeController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Like;
+
+class LikeController extends Controller
+{
+    //
+}
