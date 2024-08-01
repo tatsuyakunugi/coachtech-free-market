5b9799b45ea334d5f2a560001da64346e9a3e801
tatsuya <quercus.acutissima.tree@gmail.com>
2024-08-01 17:33:02 +0900

    シーダーファイル記述修正

--- src/database/seeders/CategoriesTableSeeder.php
+++ src/database/seeders/CategoriesTableSeeder.php
@@ -14,6 +14,101 @@ class CategoriesTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $param = [
+            'name' => '洋服',
+        ];
+        DB::table('categories')->insert($param);
+        $param = [
+            'name' => 'メンズ',
+        ];
+        DB::table('categories')->insert($param);
+        $param = [
+            'name' => 'レディース',
+        ];
+        DB::table('categories')->insert($param);
+        $param = [
+            'name' => 'トップス',
+        ];
+        DB::table('categories')->insert($param);
+        $param = [
+            'name' => 'ジャケット・アウター',
+        ];
+        DB::table('categories')->insert($param);
+        $param = [
+            'name' => 'パンツ',
+        ];
+        DB::table('categories')->insert($param);
+        $param = [
+            'name' => 'スカート',
+        ];
+        DB::table('categories')->insert($param);
+        $param = [
+            'name' => 'ワンピース',
+        ];
+        DB::table('categories')->insert($param);
+        $param = [
+            'name' => '靴',
+        ];
+        DB::table('categories')->insert($param);
+        $param = [
+            'name' => 'バッグ',
+        ];
+        DB::table('categories')->insert($param);
+        $param = [
+            'name' => '時計',
+        ];
+        DB::table('categories')->insert($param);
+        $param = [
+            'name' => '帽子',
+        ];
+        DB::table('categories')->insert($param);
+        $param = [
+            'name' => 'アクセサリー',
+        ];
+        DB::table('categories')->insert($param);
+        $param = [
+            'name' => 'スーツ',
+        ];
+        DB::table('categories')->insert($param);
+        $param = [
+            'name' => 'ルームウェア・パジャマ',
+        ];
+        DB::table('categories')->insert($param);
+        $param = [
+            'name' => '下着・アンダーウェア',
+        ];
+        DB::table('categories')->insert($param);
+        $param = [
+            'name' => 'レッグウェア',
+        ];
+        DB::table('categories')->insert($param);
+        $param = [
+            'name' => '水着・ラッシュガード',
+        ];
+        DB::table('categories')->insert($param);
+        $param = [
+            'name' => '学生服',
+        ];
+        DB::table('categories')->insert($param);
+        $param = [
+            'name' => '着物・浴衣',
+        ];
+        DB::table('categories')->insert($param);
+        $param = [
+            'name' => 'ヘアアクセサリー',
+        ];
+        DB::table('categories')->insert($param);
+        $param = [
+            'name' => 'ウィッグ・エクステ',
+        ];
+        DB::table('categories')->insert($param);
+        $param = [
+            'name' => 'マタニティー',
+        ];
+        DB::table('categories')->insert($param);
+        $param = [
+            'name' => 'ベビー・キッズ',
+        ];
+        DB::table('categories')->insert($param);
     }
 }
