9934d7d78c6344d21fb1c3236a27af5034d28d80
tatsuya <quercus.acutissima.tree@gmail.com>
2024-08-01 17:29:01 +0900

    ビューファイルに渡す変数追加

--- src/app/Http/Controllers/ItemController.php
+++ src/app/Http/Controllers/ItemController.php
@@ -3,11 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Item;
 
 class ItemController extends Controller
 {
     public function index()
     {
-        return view('index');
+        $items = Item::all();
+        return view('index', compact('items'));
+    }
+
+    public function item($id)
+    {
+        $item = Item::find($id);
+        return view('item', compact('item'));
     }
 }
