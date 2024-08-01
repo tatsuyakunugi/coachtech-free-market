03db539b99d5f6b7d0f41a20ce726c58d883c25c
tatsuya <quercus.acutissima.tree@gmail.com>
2024-08-01 17:29:40 +0900

    出品アクション作成

--- src/app/Http/Controllers/SellController.php
+++ src/app/Http/Controllers/SellController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Models\Item;
+use App\Models\Category;
+use App\Models\Condition;
 use App\Http\Requests\SellRequest;
 
 class SellController extends Controller
@@ -11,11 +14,39 @@ class SellController extends Controller
     public function sell()
     {
         $user = Auth::user();
-        return view('sell', compact('user'));
+        $categories = Category::all();
+        $conditions = Condition::all();
+
+        return view('sell', compact('categories', 'conditions'));
     }
 
     public function store(SellRequest $request)
     {
-        return redirect('sell');
+        $this->validate($request,[
+            'image' => 'required',
+            'category' => 'required',
+            'condition' => 'required',
+            'name' => 'required',
+            'description' => 'required|max:255',
+            'price' => 'required|integer|min:1',
+        ]);
+
+        $user = Auth::user();
+        $image = $request->file('image');
+        $image_path = $image->store('public/items');
+
+        $item = new Item([
+            'user_id' => $user->id,
+            'condition_id' => $request->input('condition'),
+            'image_path' => $image_path,
+            'name' => $request->input('name'),
+            'description' => $request->input('description'),
+            'price' => $request->input('price'),
+        ]);
+
+        $item->save();
+        $item->categories()->attach($request->category);
+        
+        return redirect('sell')->with('message', '出品手続きが完了しました');
     }
 }
