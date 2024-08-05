a99dcb47b2c3d90383248b5c7582bbe88037523f
tatsuya <quercus.acutissima.tree@gmail.com>
2024-08-05 17:14:23 +0900

    コメントページ表示アクション追加

--- src/app/Http/Controllers/CommentController.php
+++ src/app/Http/Controllers/CommentController.php
@@ -4,9 +4,19 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Models\Item;
 use App\Models\Comment;
 
 class CommentController extends Controller
 {
-    //
+    public function comment($id)
+    {
+        $user = Auth::user();
+        $item = Item::find($id);
+        $comments = Comment::with(['user', 'replies', 'replies.user'])
+            ->where('comments.item_id', $id)
+            ->get();
+        
+        return view('comment', compact('user', 'item', 'comments'));
+    }
 }
