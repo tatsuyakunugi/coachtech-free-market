d6ccbb3c43af73e2dce356a012965fd1d0a53c99
tatsuya <quercus.acutissima.tree@gmail.com>
2024-08-19 14:33:02 +0900

    public const ADNIN_HOMEを追記

--- src/app/Providers/RouteServiceProvider.php
+++ src/app/Providers/RouteServiceProvider.php
@@ -18,6 +18,7 @@ class RouteServiceProvider extends ServiceProvider
      * @var string
      */
     public const HOME = '/home';
+    public const ADNIN_HOME = '/admin';
 
     /**
      * The controller namespace for the application.
