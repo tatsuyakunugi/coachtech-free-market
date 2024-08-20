b75ccc194dd66d4bbd64bd08b84afcdc35056fda
tatsuya <quercus.acutissima.tree@gmail.com>
2024-08-20 17:45:22 +0900

    ユーザー情報表示アクション追加

--- src/app/Http/Controllers/AdminUserController.php
+++ src/app/Http/Controllers/AdminUserController.php
@@ -3,11 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
 
 class AdminUserController extends Controller
 {
     public function show()
     {
         return view('admin.dashboard');
+    }
+
+    public function getUserList()
+    {
+        $users = User::all();
+        return view('admin.user_list', compact('users'));
+    }
+
+    public function getUserDetail($id)
+    {
+        $user = User::find($id);
+        return view('admin.user_detail', compact('user'));
     }
 }
