7b7547e0a3aeb4a9656af0d44da4df268358fffd
tatsuya <quercus.acutissima.tree@gmail.com>
2024-08-22 11:56:23 +0900

    ユーザー情報修正

--- src/resources/views/admin/user_detail.blade.php
+++ src/resources/views/admin/user_detail.blade.php
@@ -7,46 +7,51 @@
 @section('content')
 <div class="detail-page__content">
     <div class="detail-page__heading">
-        <a href="/admin/user_list"><</a>
-        <p>ユーザー詳細</p>
+        <a class="detail-page__link" href="/admin/user_list"><</a>
+        <p>{{ $user->profile->name }}さんのユーザー詳細</p>
     </div>
-    <div class="detail-table">
-        <table class="detail-table__inner">            
-            <tr class="detail-table__row">
-                <th class="detail-table__title">名前：</th>
-            </tr>
-            <tr class="detail-table__row">
-                <th class="detail-table__title">メール：</th>
-            </tr>
-            <tr class="detail-table__row">
-                <th class="detail-table__title">住所：</th>
-            </tr>
-            <tr class="detail-table__row">
-                <th class="detail-table__title">登録日：</th>
-            </tr>
-            <tr class="detail-table__row">
-                <th class="detail-table__title"></th>
-            </tr>
-            <tr class="detail-table__row">
-                <td class="detail-table__item">{{ $user->profile->name }}</td>
-            </tr>
-            <tr class="detail-table__row">
-                <td class="detail-table__item">{{ $user->email }}</td>
-            </tr>
-            <tr class="detail-table__row">
-                <td class="detail-table__item">{{ $user->profile->address }}{{ $user->profile->building }}</td>
-            </tr>
-            <tr class="detail-table__row">
-                <td class="detail-table__item">{{ $user->created_at->format('Y-m-d H:i') }}</td>
-            </tr>
-            <tr class="detail-table__row">
-                <td class="detail-table__item">
-                    <form action="" method="">
-                        <button>削除する</button>
-                    </form>
-                </td>
-            </tr>
-        </table>  
-    </div>                
+    <div class="detail-card">
+        <div class="detail-card__inner">
+            <div class="detail-card__group">
+                <div class="detail-card__title">
+                    <p>名前：</p>
+                </div>
+                <div class="detail-card__item">
+                    {{ $user->profile->name }}
+                </div>
+            </div>
+            <div class="detail-card__group">
+                <div class="detail-card__title">
+                    <p>メール：</p>
+                </div>
+                <div class="detail-card__item">
+                    {{ $user->email }}
+                </div>
+            </div>
+            <div class="detail-card__group">
+                <div class="detail-card__title">
+                    <p>住所：</p>
+                </div>
+                <div class="detail-card__item">
+                    {{ $user->profile->address }}{{ $user->profile->building }}
+                </div>
+            </div>
+            <div class="detail-card__group">
+                <div class="detail-card__title">
+                    <p>登録日：</p>
+                </div>
+                <div class="detail-card__item">
+                    {{ $user->created_at->format('Y-m-d H:i') }}
+                </div>
+            </div>
+            <div class="delete-form__button">
+                <form class="delete-form" action="{{ route('admin.userDestroy', $user->id) }}" method="post">
+                    @method('DELETE')
+                    @csrf
+                    <button class="delete-form__button-submit" type="submit">このユーザーを削除する</button>
+                </form>
+            </div>
+        </div>
+    </div>               
 </div>
 @endsection
