c4053bb9860a78ec2d245e5a9b115bd2cfce1378
tatsuya <quercus.acutissima.tree@gmail.com>
2024-08-23 17:59:30 +0900

    記述修正

--- src/resources/views/admin/dashboard.blade.php
+++ src/resources/views/admin/dashboard.blade.php
@@ -9,10 +9,21 @@
     <div class="top-page__heading">
         <p>管理ページTOP</p>
     </div>
-    <div class="link-form">
-        <a class="link-form__button" href="{{ route('admin.showUserList') }}">
-            ユーザー一覧
-        </a>
+    <div class="top-page__body">
+        <div class="link-form">
+            <div class="user-list__link">
+                <a class="link-form__button" href="{{ route('admin.showUserList') }}">
+                    ユーザー一覧
+                </a>
+            </div>
+        </div>
+        <div class="link-form">
+            <div class="mail-create__link">
+                <a class="link-form__button" href="{{ route('admin.mailCreate') }}">
+                    メール作成
+                </a>
+            </div>
+        </div>
     </div>
 </div>
 @endsection
